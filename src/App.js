354dc16e683a7516e1c6d72e9ede810667449ba7
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-17 07:42:28 +0600

    Loop Over and Display Data with JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import users from './assets/users-data.js'
 
 function App() {
   return (
     <div className='App'>
-        <h1>Something Test for branch</h1>
+       {
+        users.map((user,index)=>{
+          return (
+            <p className="user">
+              <h3>{user.name}</h3>
+              <p>{user.location}</p>
+              <p>{user.car}</p>
+            </p>
+          )
+          
+        })
+       }
     </div>
   )
 }
---
+++ src/assets/users-data.js
@@ -0,0 +1,30 @@
+// Data export
+const users =  [
+    {
+      name: "😃 Nisha Dass",
+      location: "🏘️ Lagos",
+      car: "🚘 Honda"
+    },
+    {
+      name: "😃 Haimi Bhasin",
+      location: "🏘️ Moon",
+      car: "🚘 Tesla"
+    },
+    {
+      name: " 😃 Hrithik Saha",
+      location: "🏘️ Venice",
+      car: "🚘 Pagani"
+    },
+    {
+      name: "😃 Kshitij Brahmbhatt",
+      location: "🏘️ Milan",
+      car: "🚘 Rolls Royce"
+    },
+    {
+      name: "😃 Sudarshan Ratta",
+      location: "🏘️ Beirut",
+      car: "🚘 Mercedes"
+    }
+  ];
+  
+  export default users;
