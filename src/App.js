0a894c1f2ccc54c13097f880976176385c66900d
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-20 11:06:55 +0600

    Fizz Buzz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cnt,setCnt] = useState(1) 
   return (
     <div className='App'>
-        <h1>Something Test for branch</h1>
+        <h1>React Fizz Buzz</h1>
+        <hr/>
+        <span>
+        Counting incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz".
+        if both then 'FizzBuzz'
+        </span>
+        <h2>
+          {
+            cnt%3===0 && cnt%5 ===0
+            ?'FizzBuzz'
+            :cnt%3===0
+            ?'Fizz'
+            :cnt%5===0
+            ?'Buzz'
+            :cnt
+          }
+        </h2>
+        <button onClick={()=>setCnt(prevState=>prevState + 1)}>+</button>
+        <button onClick={()=>setCnt(prevState=>prevState - 1)}>-</button>
     </div>
   )
 }
