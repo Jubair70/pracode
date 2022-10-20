1e986d5ba6d3dc237eb33fd808cade4c84e64f49
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-20 10:09:03 +0600

    Search using FetchAPi and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React,{useEffect, useState} from 'react'
 import './App.css';
-
+let people = [];
 function App() {
-  let people = [];
+ 
   const [searchedText,setSearchedText] = useState('')
   const [filteredList, setFilteredList] = useState(people);
 
