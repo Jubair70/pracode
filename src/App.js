cbb2dd012c280f68232bb879e4c454c1eb6fbca4
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-20 09:51:49 +0600

    Search using FetchAPi and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React,{useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  let people = [];
+  const [searchedText,setSearchedText] = useState('')
+  const [filteredList, setFilteredList] = useState(people);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(result=>{
+      result.map((elem)=>people.push(elem.name))
+      const filtered = people.filter((elem)=>elem.toLowerCase().includes(searchedText.toLowerCase()))
+      setFilteredList(filtered)
+    })
+  },[searchedText])
+
+  const searchHandler = (e) =>{
+    setSearchedText(e.target.value);
+  }
   return (
     <div className='App'>
-        <h1>Something Test for branch</h1>
+        <input type="text" onChange={(e)=>searchHandler(e)}/>
+        <ul>
+          {
+            filteredList.map((elem,index) => {
+                return <li key={index}>{elem}</li>           
+            })
+          }
+        </ul>
     </div>
   )
 }
-
 export default App;
