c0a2adef09b8e302556c03c2bbf2ccc774f472cd
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-21 21:33:54 +0600

    Show or hide element in React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [show,setShow] = useState(true)
   return (
     <div className='App'>
-        <h1>Something Test for branch</h1>
+        <button onClick={()=>setShow((prevState)=>!prevState
+        )}>Toggle:{show?'Hide':'Show'}</button>
+        {
+          show && <h1>Hello world</h1>
+        }
     </div>
   )
 }
