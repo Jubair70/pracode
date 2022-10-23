f70a76b1c2f1d2aac628921bf09e0d1ef673367b
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-23 20:41:46 +0600

    fetch and Pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [users,setUsers] = useState([]);
+  let cnt = 1;
+
+  const getFirstName = (userInfo)=>{
+     const {name: {title,first,last} } = userInfo;
+     return `${title} ${first} ${last}`
+  }
+
+  const getUsersData=(page)=>{
+    fetch(`https://randomuser.me/api?page=${page}`)
+    .then(response=>response.json())
+    .then(res=>
+      {
+        
+        setUsers(prevState=>[...prevState,...res.results])
+      })
+    .catch(error=>console.log(error))
+  }
+
+  useEffect(()=>{
+    getUsersData(cnt);
+  },[cnt])
+
+
   return (
     <div className='App'>
-        <h1>Something Test for branch</h1>
+
+       <hr/>
+       <button onClick={()=>getUsersData(cnt++)}>Load More</button>
+       <hr/>
+       {
+       
+       users.map((user,index)=>{
+        
+        return (
+          <div className='user' key={Math.random()}>
+            <img src={user.picture.large} alt="No pic"/>
+            <div>
+            <strong>{getFirstName(user)}</strong>
+              </div>
+
+          </div>
+          
+        )
+       })
+       
+       }
     </div>
   )
 }
